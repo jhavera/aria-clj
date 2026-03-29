dab1ad0ad7586d0588a7ec5a9fadb4bdbbcc76f6
jhavera <jhavera@gmail.com>
2026-03-29 02:00:30 -0400

    Bump version to 0.2.0

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'com.github.jhavera/aria-clj)
-(def version "0.1.0")
+(def version "0.2.0")
 (def class-dir "target/classes")
 (def uber-file (format "target/aria-clj-%s.jar" version))
 (def basis (delay (b/create-basis {:project "deps.edn"})))
